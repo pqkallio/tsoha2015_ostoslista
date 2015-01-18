3725ce7783c35646ead4dc60d88c902451590396
Petri Kallio <petrikallio@host-002.elisa>
2015-01-18 22:34:18 +0200

    Dokumentaatio lisätty

--- app/controllers/hello_word_controller.php
+++ app/controllers/hello_word_controller.php
@@ -3,11 +3,11 @@
   class HelloWorldController extends BaseController{
 
     public static function index(){
-   	  self::render_view('home.html');
+        echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä	
-      echo 'Hello World!';
+      self::render_view('helloworld.html');
     }
   }
