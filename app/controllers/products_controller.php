57849650cbb3237dfe81ec2576b6fb5b234cab35
Petri Kallio <kallionpetri@gmail.com>
2015-02-15 23:02:26 +0200

    Deadline vko 5

--- app/controllers/products_controller.php
+++ app/controllers/products_controller.php
@@ -1,7 +1,15 @@
 <?php
 
+/**
+ * A controller class to handle requests relating to {@link Product} model.
+ * 
+ * @author Petri Kallio <kallionpetri@gmail.com>
+ */
 class ProductController extends BaseController {
     
+    /**
+     * Prepare variables needed to render the user's <em>all products</em> view and render it.
+     */
     public static function index() {
         self::check_logged_in();
         
@@ -18,6 +26,9 @@ class ProductController extends BaseController {
             'product_units' => $product_units, 'product_departments' => $product_departments));
     }
     
+    /**
+     * Prepare variables needed to render the user's <em>favorite products</em> view and render it.
+     */
     public static function favorites() {
         self::check_logged_in();
         
@@ -34,6 +45,11 @@ class ProductController extends BaseController {
             'product_units' => $product_units, 'product_departments' => $product_departments));
     }
 
+    /**
+     * Takes a {@link Product}'s id as a parameter, asks for the corresponding object from the model class and renders its <em>show</em> view.
+     * 
+     * @param integer $id an id of a particular {@link Product} object
+     */
     public static function show($id) {
         self::check_logged_in();
         $product = self::check_ownership_and_existence($id);
@@ -49,6 +65,11 @@ class ProductController extends BaseController {
         }
     }
     
+    /**
+     * Takes a {@link Product}'s id as a parameter, asks for the corresponding object from the model class and renders its <em>edit</em> view.
+     * 
+     * @param integer $id an id of a particular {@link Product} object
+     */
     public static function edit($id) {
         self::check_logged_in();
         $product = self::check_ownership_and_existence($id);
@@ -63,6 +84,11 @@ class ProductController extends BaseController {
         }
     }
     
+    /**
+     * Takes a {@link Product}'s id as a parameter, asks the model class to add the corresponding product to the signed-in user's favorite products and redirects to the user's <em>all products</em> view.
+     * 
+     * @param integer $id an id of a particular {@link Product} object
+     */
     public static function fave($id) {
         self::check_logged_in();
         $product = self::check_ownership_and_existence($id);
@@ -73,7 +99,12 @@ class ProductController extends BaseController {
         
         self::redirect_to('/products');
     }
-
+    
+    /**
+     * Takes a {@link Product}'s id as a parameter, asks the model class to delete the corresponding row from the database and redirects to the user's <em>all products</em> view.
+     * 
+     * @param integer $id an id of a particular {@link Product} object
+     */
     public static function destroy($id) {
         self::check_logged_in();
         $product = self::check_ownership_and_existence($id);
@@ -87,6 +118,9 @@ class ProductController extends BaseController {
         }
     }
     
+    /**
+     * Prepares the variables needed to render the <em>create new product</em> view and renders it.
+     */
     public static function create() {
         self::check_logged_in();
         $units = Unit::all();
@@ -95,6 +129,18 @@ class ProductController extends BaseController {
         self::render_view('product/new.html', array('units' => $units, 'departments' => $departments));
     }
     
+    /**
+     * Asks for the {@link Product} model class to insert a new row to the database based on the parameters given with HTTP Post request and either
+     * <ol>
+     *  <li>redirects to the newly added {@link Product}'s <em>show</em> view or</li>
+     *  <li>if the new object didn't pass the validations, renders the <em>create new product</em> view displaying the errors that prohibited the object from being saved</li>
+     * </ol>
+     * 
+     * @see Product::validate_name()
+     * @see Product::validate_department()
+     * @see Product::validate_unit()
+     * @see Product::validate_owner()
+     */
     public static function store() {
         self::check_logged_in();
         $params = $_POST;
@@ -120,6 +166,19 @@ class ProductController extends BaseController {
         
     }
     
+    /**
+     * Asks for the {@link Product} model class to update the row in the database with the corresponding id based on the parameters given with HTTP Post request and either
+     * <ol>
+     *  <li>redirects to the updated {@link Product}'s <em>show</em> view or</li>
+     *  <li>if the new object didn't pass the validations, renders the {@link Product}'s <em>edit product</em> view displaying the errors that prohibited the update from being made</li>
+     * </ol>
+     * 
+     * @see Product::validate_name()
+     * @see Product::validate_department()
+     * @see Product::validate_unit()
+     * @see Product::validate_owner()
+     * @param integer $id
+     */
     public static function update($id) {
         self::check_logged_in();
         $params = $_POST;
@@ -146,7 +205,13 @@ class ProductController extends BaseController {
         }
     }
     
-    public static function check_ownership_and_existence($product_id) {
+    /**
+     * A helper function to check the signed-in user's ownership to the {@link Product} which id's given as parameter as well as the existence of the product
+     * 
+     * @param integer $product_id the id of a {@link Product} object
+     * @return null or {@link Product}
+     */
+    private static function check_ownership_and_existence($product_id) {
         $product = Product::find($product_id);
         
         if (!parent::get_user_logged_in()) {
--- app/models/product.php
+++ app/models/product.php
@@ -1,13 +1,18 @@
 <?php
 
 /**
- * Description of Product
+ * This model is an abstraction of a single product used to form a {@link Purchase}.
  *
  * @author kallionpetri
  */
 class Product extends BaseModel {
     public $id, $name, $department, $unit, $owner, $favorite;
     
+    /**
+     * A function that validates the Product's name.
+     * 
+     * @return array an array of error messages as strings
+     */
     public function validate_name() {
         $errors = array();
         
@@ -31,6 +36,11 @@ class Product extends BaseModel {
         return $errors;
     }
     
+    /**
+     * A function that validates the Product's department.
+     * 
+     * @return array an array of error messages as strings
+     */
     public function validate_department() {
         $errors = array();
         
@@ -47,6 +57,11 @@ class Product extends BaseModel {
         return $errors;
     }
     
+    /**
+     * A function that validates the Product's unit.
+     * 
+     * @return array an array of error messages as strings
+     */
     public function validate_unit() {
         $errors = array();
         
@@ -63,6 +78,11 @@ class Product extends BaseModel {
         return $errors;
     }
     
+    /**
+     * A function that validates the Product's owner.
+     * 
+     * @return array an array of error messages as strings
+     */
     public function validate_owner() {
         $errors = array();
         
@@ -75,6 +95,11 @@ class Product extends BaseModel {
         return $errors;
     }
     
+    /**
+     * A constructor inherited from {@link BaseModel}
+     * 
+     * @param array $attributes attributes used to construct an object (default null)
+     */
     public function __construct($attributes = null) {
         parent::__construct($attributes);
         $this->favorite = false;
@@ -83,23 +108,28 @@ class Product extends BaseModel {
             'validate_unit', 'validate_owner');
     }
     
+    /**
+     * Retrieves all rows from the table <em>Product</em> and constructs Product objects from them finally returning them as an array.
+     * 
+     * @return array an array of Product objects
+     */
     public static function all() {
         $products = array();
         $rows = DB::query('SELECT * FROM Product ORDER BY name ASC');
         
         foreach ($rows as $row) {
-            $products[] = new Product(array(
-                'id' => $row['id'],
-                'name' => $row['name'],
-                'department' => $row['department'],
-                'unit' => $row['unit'],
-                'owner' => $row['owner']
-            ));
+            $products[] = self::create_product($row);
         }
         
         return $products;
     }
     
+    /**
+     * Finds a row from the table <em>Product</em> with the id given as parameter, constructs a Product object of it and finally returns it.
+     * 
+     * @param integer $id
+     * @return \Product the Product object corresponding to the id
+     */
     public static function find($id) {
         $product = null;
         $rows = DB::query('SELECT * FROM Product WHERE id = :id LIMIT 1', array('id' => $id));
@@ -107,64 +137,58 @@ class Product extends BaseModel {
         if (count($rows) > 0) {
             $row = $rows[0];
             
-            $product = new Product(array(
-                'id' => $row['id'],
-                'name' => $row['name'],
-                'department' => $row['department'],
-                'unit' => $row['unit'],
-                'owner' => $row['owner'],
-                'favorite' => false
-            ));
+            $product = self::create_product($row);
         }
         
         return $product;
     }
 
-    public static function find_by_name($user, $name) {
+    /**
+     * Queries the database for a row corresponding to a user's id and the name of product, constructs an object and finally returns it.
+     * 
+     * @param integer $user_id
+     * @param string $name
+     * @return \Product
+     */
+    public static function find_by_name($user_id, $name) {
         $product = null;
         $rows = DB::query('SELECT * FROM Product '
                         . 'WHERE owner = :owner AND name = :name LIMIT 1',
-                array('owner' => $user, 'name' => $name));
+                array('owner' => $user_id, 'name' => $name));
         
         if (count($rows) > 0) {
             $row = $rows[0];
             
-            $product = new Product(array(
-                'id' => $row['id'],
-                'name' => $row['name'],
-                'department' => $row['department'],
-                'unit' => $row['unit'],
-                'owner' => $row['owner']
-            ));
-            
-            $product->favorite = self::is_favorite($product->id, $product->owner);
+            $product = self::create_product($row);
         }
         
         return $product;
     }
     
-    public static function find_by_owner($user) {
+    /**
+     * Queries the database for all rows from the table <em>Product</em> that have the user's id as owner, constructs objects out of them and returns them as an array.
+     * 
+     * @param integer $user_id the user's id whose products are to be found
+     * @return array an array of Product objects
+     */
+    public static function find_by_owner($user_id) {
         $products = array();
         $rows = DB::query('SELECT * FROM Product WHERE owner = :owner ORDER BY name ASC',
-                array('owner' => $user));
+                array('owner' => $user_id));
         
         foreach ($rows as $row) {
-            $products[] = new Product(array(
-                'id' => $row['id'],
-                'name' => $row['name'],
-                'department' => $row['department'],
-                'unit' => $row['unit'],
-                'owner' => $row['owner']
-            ));
-        }
-        
-        foreach ($products as $product) {
-            $product->favorite = self::is_favorite($product->id, $product->owner);
+            $products[] = self::create_product($row);
         }
         
         return $products;
     }
     
+    /**
+     * Returns a user's favorite products.
+     * 
+     * @param array $params
+     * @return array an array of the user's favorite products
+     */
     public static function find_owners_favorites($params) {
         $users_products = self::find_by_owner($params['user']);
         $users_favorites = array();
@@ -178,6 +202,12 @@ class Product extends BaseModel {
         return $users_favorites;
     }
     
+    /**
+     * Inserts a new row to the table <em>Product</em> based upon the params. Returns the id of the new row.
+     * 
+     * @param array $params
+     * @return integer the new products id
+     */
     public static function create($params) {
         $rows = DB::query('INSERT INTO Product (name, department, unit, owner) '
                 . 'VALUES (:name, :department, :unit, :owner) RETURNING id',
@@ -190,21 +220,39 @@ class Product extends BaseModel {
         return $last_id['id'];
     }
     
-    public static function update($id, $attributes) {
+    /**
+     * Updates the <em>Product</em> table's row corresponding to the id given as parameter with the values given in the $params array.
+     * 
+     * @param integer $id
+     * @param array $params
+     */
+    public static function update($id, $params) {
         DB::query('UPDATE Product '
                 . 'SET name = :name, department = :department, unit = :unit '
                 . 'WHERE id = :id', 
-                array('name' => $attributes['name'],
-                      'department' => $attributes['department'],
-                      'unit' => $attributes['unit'],
+                array('name' => $params['name'],
+                      'department' => $params['department'],
+                      'unit' => $params['unit'],
                       'id' => $id));
     }
-
+    
+    /**
+     * Deletes the row of the table <em>Product</em> corresponding to the id given as parameter.
+     * 
+     * @param integer $id
+     */
     public static function delete($id) {
         DB::query('DELETE FROM Product WHERE id = :id', array('id' => $id));
     }
     
-    public static function is_favorite($product_id, $user_id) {
+    /**
+     * Queries the table <em>Favorite_product</em> for a corresponding row based on $product_id and $user_id and returns true if it exists and false otherwise.
+     * 
+     * @param integer $product_id
+     * @param integer $user_id
+     * @return boolean
+     */
+    private static function is_favorite($product_id, $user_id) {
         $rows = DB::query('SELECT * FROM Favorite_product WHERE product = :product_id '
                 . 'AND user_id = :user_id LIMIT 1', array('product_id' => $product_id, 
                     'user_id' => $user_id));
@@ -216,6 +264,12 @@ class Product extends BaseModel {
         return true;
     }
     
+    /**
+     * Either inserts or deletes a row of the table <em>Favorite_product</em> based on whether a row with $product_id and $user_id already exists or not.
+     * 
+     * @param type $product_id
+     * @param type $user_id
+     */
     public static function fave($product_id, $user_id) {
         if (self::is_favorite($product_id, $user_id)) {
             DB::query('DELETE FROM Favorite_product '
@@ -226,5 +280,27 @@ class Product extends BaseModel {
                     . 'VALUES (:product_id, :user_id)',
                     array('product_id' => $product_id, 'user_id' => $user_id));
         }
+    }
+    
+    /**
+     * Creates a <em>Product</em> object based on a row from the table <em>Product</em>.
+     * 
+     * @param type $row
+     * @return \Product
+     */
+    private static function create_product($row) {
+        $product = new Product(array(
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'department' => $row['department'],
+                'unit' => $row['unit'],
+                'owner' => $row['owner']
+            ));
+        
+        if ($product) {
+            $product->favorite = self::is_favorite($product->id, $product->owner);
+        }
+        
+        return $product;
     }
 }
